d6ac6493f33c64c0acd56d566b84c2f411828f35
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-08-18 15:02:32 +0600

    save

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -1,4 +1,3 @@
-import { ReadDirSize } from "../../lib/utils/utils.js";
 import FileService from "../services/file.service.js";
 
 const MB = 1e+6;
@@ -9,16 +8,10 @@ class  FileController {
     }
     async addNewFile(req,res) {
         try{
-            const name = req.params.name;
+            const name = req.params.fileName;
             const { data, ...meta } = req.file
             const file = await this.service.saveFile(name,data,meta)
-            ReadDirSize().then(size => {
-                if (Number(req.file.size) + Number(size) > MB * 10) {
-					res.send( { status: 'ok', Warning:'Attention, the directory size is more than 10 megabytes' } );
-				} else {
-					res.send(file);
-				}
-            })
+            res.send(file);
         }catch (e) {
             res.status(500).json(e)
         }
